d2a80aedf5d8df3c962f65f5c271ae331209fbda
unnipv <unnimonpv@gmail.com>
2022-05-07 18:23:31 +0530

    Something

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ from dataset.droplets import DropletDataset
 from torch.utils.data import DataLoader
 from model.Unninet import UnniNet
 import torch
+import torchvision
+from PIL import Image
 import sys
+import os
 
 ROOT_DIR = "data/m_crop"
 TEST_CSV = "data/m_crop/test.csv"
@@ -22,12 +25,27 @@ model.load_state_dict(torch.load(modelPath)) # Load trained model
 model.eval() # Set net to evaluation mode, usually usefull in this case its fail
 
 #----------------Make preidction--------------------------------------------------------------------------
-for data, target in test_dataloader:
-    data, target = data.to(device), target.to(device)           
-    preds = model(data)
-    outputs = torch.sigmoid(preds)
-    outputs = outputs * 1400
+# for data, target in test_dataloader:
+#     data, target = data.to(device), target.to(device)           
+#     preds = model(data)
+#     outputs = torch.sigmoid(preds)
+#     outputs = outputs * 1400
 
-print("Predictions //n")
-print(outputs)
+# print("Predictions //n")
+# print(outputs)
+
+#individual image
+default_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Grayscale(num_output_channels = 1),
+    torchvision.transforms.Normalize((0.5), (0.5)),
+    ]
+)
+img_path = sys.argv[2]
+image = Image.open(img_path)
+image = default_transforms(image)
+pred = model(image)
+print(torch.sigmoid(pred)*1400)
+
+
 
